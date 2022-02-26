c9f9cc77a68936ced755fbd2d1ad2010b3c30b19
Xeno Mannens <Xeno.mannens@me.com>
2022-02-26 17:49:21 +0100

    project init

---
+++ BPSwiftUI/BPSwiftUI/BPSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPSwiftUIApp.swift
+//  BPSwiftUI
+//
+//  Created by Xeno Mannens on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BPSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
